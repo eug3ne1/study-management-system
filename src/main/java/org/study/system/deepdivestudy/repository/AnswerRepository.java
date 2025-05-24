126ea4d00e5f5724250fe5538ae226a109f9c4b6
eug3ne1 <miholatevgen@gmail.com>
2025-05-24 11:13:02 +0300

    delete unnecessary files

--- src/main/java/org/study/system/deepdivestudy/repository/AnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package org.study.system.deepdivestudy.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.study.system.deepdivestudy.entity.testing.Answer;
-
-public interface AnswerRepository extends JpaRepository<Answer,Long> {
-}
